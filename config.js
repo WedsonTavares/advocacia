f9ec0df81006adb224f82b0a1709896ebb5a4243
WedsonTavares <wedsonsobral@gmail.com>
2026-04-20 17:26:40 -0300

    chore: versiona config runtime para producao

---
+++ config.js
@@ -0,0 +1,12 @@
+window.APP_CONFIG = {
+  "CHATWOOT_TOKEN": "",
+  "CHATWOOT_URL": "",
+  "N8N_WEBHOOK_URL": "https://n8n.zeus-estoque.com.br/webhook/atendimento-juridico",
+  "ADMIN_CONVERSAS_ENDPOINT": "",
+  "ADMIN_LOGS_ENDPOINT": "",
+  "SUPABASE_URL": "https://kkvrrzmlbvytdxjvrrrg.supabase.co",
+  "SUPABASE_ANON_KEY": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtrdnJyem1sYnZ5dGR4anZycnJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY2MzUwMTgsImV4cCI6MjA5MjIxMTAxOH0.0dRCZJVdyz1K5VKD07htBHBATYF0YyLvSeT2JmirMiE",
+  "SUPABASE_LEADS_TABLE": "chat_leads",
+  "SUPABASE_CONVERSAS_TABLE": "",
+  "SUPABASE_LOGS_TABLE": ""
+};
